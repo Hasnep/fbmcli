f113cc9a80bb03456617cf8ae32f9e727e0854d6
Hannes <hasnep@gmail.com>
2018-07-31 08:56:44 +0800

    Complete rewrite of main functionality

--- fbmcli.py
+++ fbmcli.py
@@ -1,6 +1,7 @@
 from login import *
-from PyInquirer import *
+import cmd
 from pprint import pprint
+from pdb import *  # use set_trace() to debug
 from datetime import datetime
 import tzlocal
 local_timezone = tzlocal.get_localzone()  # get timezone
@@ -14,66 +15,131 @@ def timestamp_to_string(timestamp, tz=local_timezone):
     return date_string
 
 
-threads = client.fetchThreadList(limit=n_contacts_list)  # get the list of most recent threads
-threads_list = []
-for i in range(n_contacts_list):
-    cur_thread = threads[i]
-    if cur_thread.type == ThreadType.USER and cur_thread.nickname is not None:
-        threads_list.append(cur_thread.nickname + " (" + str(i) + ")")
-    else:
-        threads_list.append(cur_thread.name + " (" + str(i) + ")")
-
-# define a question to ask the user
-choose_thread = [
-    {
-        'type': 'rawlist',
-        'name': 'selected_thread',
-        'message': 'Choose a thread:',
-        'choices': threads_list
-    },
-]
-
-selected_thread = prompt(choose_thread)  # ask the user to choose a thread from a list
-selected_thread = selected_thread['selected_thread']  # get the answer as a string
-selected_thread = selected_thread[-2: -1]  # subset the string to get the index
-selected_thread = int(float(selected_thread))  # convert the string to a float then an int
-selected_thread = threads[selected_thread]  # get the ith item of the recent threads list
-selected_thread = selected_thread.uid  # get the uid of the thread
-selected_thread = client.fetchThreadInfo(selected_thread)  # get the information about the thread
-selected_thread = selected_thread.values()  # turn the dict into a view object
-selected_thread = list(selected_thread)  # turn the view object into a list object
-selected_thread = selected_thread[0]  # get the first item in the list
-
-names = dict()
-if selected_thread.type == ThreadType.USER:
-    if selected_thread.own_nickname is None:
-        names[client.uid] = "me"
-    else:
-        names[client.uid] = selected_thread.own_nickname  # TODO: make an option to have "ME:", yourname, or your nickname
-    if selected_thread.nickname is None:
-        names[selected_thread.uid] = selected_thread.name
-    else:
-        names[selected_thread.uid] = selected_thread.nickname
-elif selected_thread.type == ThreadType.GROUP:
-    for participant_uid in selected_thread.participants:
-        if participant_uid in selected_thread.nicknames:
-            names[participant_uid] = selected_thread.nicknames.get(participant_uid)
+def get_thread_names(threads_object):
+    """Input a threads object and return the list of user facing thread names."""
+    names_list = []
+    for i in range(len(threads_object)):
+        cur_thread = threads_object[i]
+        if cur_thread.type == ThreadType.USER and cur_thread.nickname is not None:
+            names_list.append(cur_thread.nickname)
         else:
-            participant_info = client.fetchUserInfo(participant_uid)
-            participant_info = participant_info[participant_uid]
-            names[participant_uid] = participant_info.name
-else:
-    print("selected thread is not a user or a group")
+            names_list.append(cur_thread.name)
+    return names_list
 
-chat = client.fetchThreadMessages(selected_thread.uid)
-chat.reverse()
 
-for message in chat:
-    print(timestamp_to_string(message.timestamp) + " " + names.get(message.author) + ": " + message.text)
+def get_thread_uids(threads_object):
+    """Input a threads object and return the list of UIDs for each thread."""
+    uids_list = []
+    for i in range(len(threads_object)):
+        cur_thread = threads_object[i]
+        uids_list.append(cur_thread.uid)
+    return uids_list
 
+
+def get_threads(n_threads):
+    """Input a number of threads and return the last n threads as a threads object."""
+    threads = client.fetchThreadList(limit=n_threads)
+    return threads
+
+
+def print_choose_a_thread(threads):
+    """Input a threads object and print a list of threads with indices next to their names."""
+    threads_names = get_thread_names(threads)
+    max_i_length = len(str(len(threads)))
+    print("Choose a thread:")
+    for i in range(len(threads)):
+        print("[" + " " * (max_i_length - len(str(i))) + str(i) + "] " + threads_names[i])
+    return
+
+
+def thread_id_input(n_threads):
+    """Input a number of threads an return the index of the chosen thread or None if an invalid index is chosen."""
+    input_string = input(option_prompt)
+    try:
+        chosen_thread_index = int(float(input_string))
+    except TypeError:
+        print("Enter a number between 0 and %s." % n_threads)
+        return None
+    else:
+        if chosen_thread_index < 0:
+            print("Enter a number between 0 and %s." % n_threads)
+            return None
+        elif chosen_thread_index > n_threads:
+            print("Enter a number between 0 and %s." % n_threads)
+            return None
+        else:
+            return chosen_thread_index
+
+
+def choose_thread(n_threads):
+    """Input the number of threads and return the UID of the chosen thread."""
+    threads = get_threads(n_threads)
+    chosen_thread_index = None
+    while chosen_thread_index is None:
+        print_choose_a_thread(threads)
+        chosen_thread_index = thread_id_input(n_threads)
+    threads_uids = get_thread_uids(threads)
+    chosen_thread_uid = threads_uids[chosen_thread_index]
+    return chosen_thread_uid
+
+
+def uid_to_chat(input_uid):
+    """Input a UID and return either a USER or a GROUP object."""
+    chat = client.fetchThreadInfo(input_uid)  # get the information about the thread
+    chat = chat.values()  # turn the dict into a view object
+    chat = list(chat)  # turn the view object into a list object
+    chat = chat[0]  # get the first item in the list
+    return chat
+
+
+def get_chat_names(chat):
+    """Input a user or a group to get a dict with everyone's UIDs and names."""
+    chat_names = dict()
+    if chat == ThreadType.USER:
+        # TODO: make an option to have "ME:", yourname, or your nickname as your representation.
+
+        # Set own name
+        my_uid = client.uid
+        if chat.own_nickname is None:
+            chat_names[my_uid] = "Me"
+        else:
+            chat_names[my_uid] = user_or_group.own_nickname
+
+        # Set the other person's name
+        if chat.nickname is None:
+            chat_names[chat.uid] = chat.name
+        else:
+            chat_names[chat.uid] = chat.nickname
+
+    elif chat == ThreadType.GROUP:
+        for k_uid in chat.participants:
+            if k_uid in chat.nicknames:
+                chat_names[k_uid] = chat.nicknames.get(k_uid)
+            else:
+                chat_names[k_uid] = participant_info[k_uid].name
+
+    else:
+        print("idk what you've done, that's not a user or a group.")
+        return None
+
+    return chat_names
+
+
+def print_chatlog(chat, chat_names=None):
+    """Print the chatlog."""
+    if chat_names is None:
+        chat_names = get_chat_names(chat)
+
+    chatlog = client.fetchThreadMessages(chat.uid)
+    chatlog.reverse()
+
+    for message in chatlog:
+        print(timestamp_to_string(message.timestamp) + " " + chat_names.get(message.author) + ": " + message.text)
+
+thread_uid = choose_thread(option_n_threads)
+selected_chat = uid_to_chat(thread_uid)
+print_chatlog(selected_chat)
 client.logout()
-
-# import pdb; pdb.set_trace()
 
 # fetchThreadMessages
 # fetchUnread()
