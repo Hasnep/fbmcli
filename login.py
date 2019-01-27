0ccba04d3d3572dbc743f121ec7aac9e11ca32e1
Hasnep <hasnep@gmail.com>
2019-01-27 22:58:38 +0000

    Catch more specific errors when logging in

--- login.py
+++ login.py
@@ -11,18 +11,17 @@ def login(_config=config):
         print("No cookies file found, logging in with username and password.")
         try:
             client = fb.Client(_config["username"], _config["password"])  # log in using cookies
-        except:
+        except ConnectionError:
             print("Could not log in using username/password.")
             return None
     else:
         try:
             client = fb.Client(_config["username"], _config["password"], session_cookies=session_cookies)  # log in using cookies
-        except:
+        except (ConnectionError, fb.models.FBchatUserError):
             print("Could not log in using cookies or username and password.")
             return None
 
     session_cookies = client.getSession()  # put cookies in a variable
     with open(_config["cookies_path"], "wb") as handle:  # save the cookies to a file
         pickle.dump(session_cookies, handle)
-
     return client
